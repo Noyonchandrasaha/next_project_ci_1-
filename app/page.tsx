f0f6ab8e86dc336f5edbaaf95aa243f38425a2ef
--get <--get>
2025-12-27 11:38:35 +0600

    update version 8

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 
 export default function Home() {
   return (
-    <div>
-      <h1>This is the home page </h1>
-      <p>new update version 3</p>
-      <p>new update version 4</p>
-      <p>Another update</p>
-      <p>Update version 5</p>
-      <p>Update version 7</p>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1>This is the home page</h1>
+        <p>new update version 3</p>
+        <p>new update version 4</p>
+        <p>Another update</p>
+        <p>Update version 5</p>
+        <p>Update version 7</p>
+      </div>
     </div>
   );
 }
